b0a2c700808e053de7255748075b57fcb3b0787a
Henrik Johansson <henrik.jg.johansson@gmail.comn>
2022-12-03 03:49:12 +0100

    Improve day01 code

--- Day01/Day01.py
+++ Day01/Day01.py
@@ -1,26 +1,15 @@
+def getCals(calList, amount):
+    sums = sorted([sum(s) for s in calList])
+    return sum(sums[-amount:])
+
 def part(input):
     with open(input) as f:
         lines = f.readlines()
-        lines = [s.strip() for s in lines]
+        l = [x.split(' ') for x in ' '.join([x.strip() for x in lines]).split('  ')]
+        calories = [[int(s) for s in x] for x in l]
 
-        calories = 0
-        elf = 1
-        myDict = {}
-        for line in lines:
-            if line == "":
-                myDict.update({elf: calories})
-                elf+=1
-                calories = 0
-            else:
-                calories += int(line)
-        if calories != 0:
-            myDict.update({elf: calories})  
+        print("part1: " + str(getCals(calories, 1)))
+        print("part2: " + str(getCals(calories, 3)))
 
-    myDict = dict(sorted(myDict.items(), key=lambda item: item[1])) 
-    res = dict(reversed(list(myDict.items())))
-    print("part1: " + str(list(res.values())[0]))
-
-    top3Cal = list(res.values())[0] + list(res.values())[1] + list(res.values())[2]
-    print("part2: " + str(top3Cal))
 
 part('Day01/input/input.txt')
