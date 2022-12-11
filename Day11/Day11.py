ac814affee6fceef8cb5308c7926b172c50d3f72
Henrik Johansson <henrik.jg.johansson@gmail.com>
2022-12-11 11:36:58 +0100

    Day11 Cleanup

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -8,6 +8,7 @@ class Monkey:
         self.trueIndex = trueIndex
         self.falseIndex = falseIndex
         self.inspections = 0
+        self.commonDivider = 3
 
     def doBusiness(self, monkeys):
         for i in range(len(self.items)):
@@ -22,57 +23,25 @@ class Monkey:
             else:
                 self.items[i] += val
             
-            newItem = self.items[i] // 3
-            if newItem % self.divTest == 0:
-                index = self.trueIndex
-            else:
-                index = self.falseIndex
-            monkeys[index].items.append(newItem)
-        self.items = []     
+            if self.commonDivider == 3: # part1
+                self.items[i] = self.items[i] // self.commonDivider
+            else:                       #part2
+                wholes = self.items[i] // self.commonDivider
+                self.items[i] -= wholes * self.commonDivider
 
-class Monkey2:
-    def __init__(self, monkeyIndex, items, op, val, divTest, trueIndex, falseIndex):
-        self.monkeyIndex = monkeyIndex
-        self.items = items
-        self.op = op
-        self.val = val
-        self.divTest = divTest
-        self.trueIndex = trueIndex
-        self.falseIndex = falseIndex
-        self.inspections = 0
-
-    def doBusiness(self, monkeys):
-        for i in range(len(self.items)):
-            self.inspections += 1
-            if self.val == "old":
-                val = self.items[i]
-            else:
-                val = int(self.val)
-
-            if self.op == "*":
-                self.items[i] *= val
-            else:
-                self.items[i] += val
-            
-            #wholes = self.items[i] // (23*19*13*17)
-            #self.items[i] -= wholes * self.divTest
             if self.items[i] % self.divTest == 0:
                 index = self.trueIndex
             else:
                 index = self.falseIndex
-            monkeys[index].addItem(self.items[i])
+            monkeys[index].items.append(self.items[i])
             
         self.items = []     
     
-    def addItem(self, item):
-        #wholes = item // (23*19*13*17)
-        #item -= wholes * (23*19*13*17)
-        wholes = item // (11*3*5*7*19*2*13*17)
-        item -= wholes * (11*3*5*7*19*2*13*17)
+    def addCommonDivider(self, commonDivider):
+        self.commonDivider = commonDivider
 
-        self.items.append(item)
-
-def readInput(lines, monkeys):
+def readInput(lines, monkeys, setCommonDivider):
+    commonDivider = 1
     i = 0
     while i < len(lines):
         monkeyIndex = int(lines[i].split()[1].strip(":"))
@@ -80,34 +49,19 @@ def readInput(lines, monkeys):
         items = list(map(int, items))
         op, val = lines[i+2][len("Operation: new = old "):].split()
         divTest = int(lines[i+3][len("Test: divisible by "):])
+        commonDivider *= divTest
         trueIndex = int(lines[i+4][len("If true: throw to monkey "):])
         falseIndex = int(lines[i+5][len("If false: throw to monkey "):])
         monkeys.append(Monkey(monkeyIndex, items, op, val, divTest, trueIndex, falseIndex))
         i += 7
-
-def readInput2(lines, monkeys):
-    i = 0
-    while i < len(lines):
-        monkeyIndex = int(lines[i].split()[1].strip(":"))
-        items = lines[i+1][len("Starting items: "):].split(", ")
-        items = list(map(int, items))
-        op, val = lines[i+2][len("Operation: new = old "):].split()
-        divTest = int(lines[i+3][len("Test: divisible by "):])
-        trueIndex = int(lines[i+4][len("If true: throw to monkey "):])
-        falseIndex = int(lines[i+5][len("If false: throw to monkey "):])
-        monkeys.append(Monkey2(monkeyIndex, items, op, val, divTest, trueIndex, falseIndex))
-        i += 7
+    if setCommonDivider: # Only in part 2
+        for monkey in monkeys:
+            monkey.addCommonDivider(commonDivider)
 
 def processMonkeyBusiness(monkeys, rounds):
     for i in range(rounds):
         for monkey in monkeys:
             monkey.doBusiness(monkeys)
-        if i in (1, 20) or (i+1) % 1000 == 0:
-            print("== After round " + str(i+1) + " ==")
-            print("Monkey 0 inspected items " + str(monkeys[0].inspections) + " times.")
-            print("Monkey 1 inspected items " + str(monkeys[1].inspections) + " times.")
-            print("Monkey 2 inspected items " + str(monkeys[2].inspections) + " times.")
-            print("Monkey 3 inspected items " + str(monkeys[3].inspections) + " times.")
 
 def findInspectionVal(monkeys):
     inspections = []
@@ -123,8 +77,8 @@ def part1(input):
         lines = [s.strip() for s in lines]
         monkeys = []
 
-        readInput(lines, monkeys)
-        processMonkeyBusiness(monkeys, 10000)
+        readInput(lines, monkeys, False)
+        processMonkeyBusiness(monkeys, 20)
         inspectionVal = findInspectionVal(monkeys)
 
         print("part1: " + str(inspectionVal))
@@ -135,15 +89,11 @@ def part2(input):
         lines = [s.strip() for s in lines]
         monkeys = []
 
-        readInput2(lines, monkeys)
+        readInput(lines, monkeys, True)
         processMonkeyBusiness(monkeys, 10000)
         inspectionVal = findInspectionVal(monkeys)
 
         print("part2: " + str(inspectionVal))
 
-
-
-
-
-#part1('Day11/input/input.txt')
-part2('Day11/input/input.txt')
+part1('Day11/input/input.txt')
+part2('Day11/input/testinput.txt')
